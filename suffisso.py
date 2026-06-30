9a0387ad533f362e9c0cceb4099f112aac0914a2
Munit-prog <munitsaro@gmail.com>
2026-06-30 14:07:23 +0200

    Suffissi di stringhe

---
+++ suffisso.py
@@ -0,0 +1,10 @@
+def suffixes(s)
+risultato = []
+for i in range(0,len(s)+1)
+def suffixes(s):
+    risultato=[];
+    for i in range(0,len(s)+1):
+        suffisso=s[i:]
+        risultato.append(suffisso)
+    return risultato
+    
